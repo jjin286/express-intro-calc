c242b6f77dc2dc17acaefb34ebffcfceeb3e137e
JaeWon Jin <j1jin286@gmail.com>
2023-10-18 11:40:00 -0700

    code review complete TODO WIP

--- app.js
+++ app.js
@@ -1,22 +1,79 @@
+"use strict";
 /** Simple demo Express app. */
 
 const express = require("express");
+const {findMean, findMedian,findMode,} = require("./stats");
 const app = express();
 
 // useful error class to throw
-const { NotFoundError } = require("./expressError");
+const { NotFoundError, BadRequestError } = require("./expressError");
 
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
 
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
+app.get("/mean", function(req, res){
+  if(!req.query.nums){
+    throw new BadRequestError("nums are required.")
+  }
+// TODO: move logic to utils function
+// TODO: check out difference between isNaN function and calling on Number class method
+  const nums = req.query.nums.split(',').map(function(x){
+    if(isNaN(Number(x))){
+      throw new BadRequestError(`${x} is not a number.`);
+    } else {
+      return Number(x);
+    }
+  });
 
+  return res.json({response: {
+    operation: "mean",
+    value: findMean(nums)
+  }});
+
+})
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
+app.get("/median", function(req, res){
+  if(!req.query.nums){
+    throw new BadRequestError("nums are required.")
+  }
 
+  const nums = req.query.nums.split(',').map(function(x){
+      if(isNaN(Number(x))){
+        throw new BadRequestError(`${x} is not a number.`);
+      } else {
+        return Number(x);
+      }
+    });
 
-/** Finds mode of nums in qs: returns {operation: "mean", result } */
+  return res.json({response: {
+    operation: "median",
+    value: findMedian(nums)
+  }});
 
+})
+
+/** Finds mode of nums in qs: returns {operation: "mode", result } */
+app.get("/mode", function(req, res){
+  if(!req.query.nums){
+    throw new BadRequestError("nums are required.")
+  }
+
+  const nums = req.query.nums.split(',').map(function(x){
+    if(isNaN(Number(x))){
+      throw new BadRequestError(`${x} is not a number.`);
+    } else {
+      return Number(x);
+    }
+  });
+
+  return res.json({response: {
+    operation: "mode",
+    value: findMode(nums)
+  }});
+
+})
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res) {
